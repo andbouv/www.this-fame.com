92ce491feccf2ab2b790412d659e24ecced43b7e
Andrew <andrew@hiboost.fr>
2018-01-23 14:12:33 +0100

    Search

---
+++ wp-content/themes/thisfame/functions/searchnew.php
@@ -0,0 +1,10 @@
+<?
+add_filter('posts_orderby', 'group_by_post_type', 10, 2);
+function group_by_post_type($orderby, $query) {
+    global $wpdb;
+    if ($query->is_search) {
+        return $wpdb->posts . '.post_type DESC';
+    }
+    // provide a default fallback return if the above condition is not true
+    return $orderby;
+}
